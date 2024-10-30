34aa745ab2277d46dae2bf3c682daf4a57161395
SebastianCasanova4 <s.casanovao@uniandes.edu.co>
2024-10-30 13:46:00 -0500

    Finished chat AI

--- lib/views/ai_helper_view.dart
+++ lib/views/ai_helper_view.dart
@@ -1,6 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:studyglide/models/message_chatAI_model.dart';
+import 'package:studyglide/services/firestore_chatIA_service.dart';
 import '../constants/constants.dart';
 import 'package:chat_bubbles/chat_bubbles.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class AiHelperView extends StatefulWidget {
   const AiHelperView({super.key});
@@ -10,21 +15,114 @@ class AiHelperView extends StatefulWidget {
 }
 
 class _AiHelperViewState extends State<AiHelperView> {
-  final List<Map<String, dynamic>> messages = [
-    {"text": "Hello, I am AI ChatBot. How can I help you?", "isUser": false}
-  ];
   final TextEditingController _controller = TextEditingController();
+  User? currentUser = FirebaseAuth.instance.currentUser;
+  final FirestoreChatiaService _fire = FirestoreChatiaService();
+  final ScrollController _scrollController = ScrollController();
 
-  void _sendMessage() {
-    if (_controller.text.isNotEmpty) {
+  final List<Map<String, dynamic>> messages = [];
+  bool firstLoad = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadMessages();
+  }
+
+  void _loadMessages() {
+    _fire.obtenerMensajes(currentUser!.uid).listen((event) {
       setState(() {
-        messages.add({"text": _controller.text, "isUser": true});
-        messages.add(
-            {"text": "Bot response", "isUser": false}); // Respuesta de la IA
+        messages.clear();
+        messages.addAll(event.map((msg) => msg.toMap()));
+        // Verifica si es la primera carga y no hay mensajes previos
+        if (firstLoad && messages.isEmpty) {
+          final primerMensaje = Mensaje(
+            senderId: "AI",
+            receiverId: currentUser!.uid,
+            message: "Hi, I am AI ChatBot. How can I help you?",
+            timestamp: DateTime.now().millisecondsSinceEpoch,
+          );
+          messages.add(primerMensaje.toMap());
+          _fire.guardarMensaje(currentUser!.uid, primerMensaje);
+        }
+        firstLoad = false;
       });
-      _controller.clear();
+      _scrollToBottom();
+    });
+  }
+
+  void _scrollToBottom() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _scrollController.jumpTo(_scrollController.position.maxScrollExtent);
+    });
+  }
+
+  void _sendMessage() async {
+  if (_controller.text.isNotEmpty) {
+    final mensaje = Mensaje(
+      senderId: currentUser!.uid,
+      receiverId: "AI",
+      message: _controller.text,
+      timestamp: DateTime.now().millisecondsSinceEpoch,
+    );
+
+    // Agrega el mensaje del usuario al chat inmediatamente
+    setState(() {
+      messages.add(mensaje.toMap());
+    });
+    _controller.clear();
+    _scrollToBottom();
+
+    try {
+      // Prueba la solicitud usando GET en lugar de POST
+      final response = await http.post(
+        Uri.parse('https://chat.notadev.lat/chat'),
+        headers: <String, String>{
+          "x-api-key": "3b82626eab615f1343d276b6ea7a95b2112b7e5a3a8400fb7fcccb00fd2b8f2f56141594a2c5503fc8db715faac59c460ae08d4ab74ca888f1fc0a25e3524af5",
+          "Content-Type": "application/json",
+        },
+        body: jsonEncode(<String, String>{
+          "content": mensaje.message,
+        }),
+      );
+
+    
+
+      // Crear mensaje de respuesta de la IA
+      final respuesta = Mensaje(
+        senderId: "AI",
+        receiverId: currentUser!.uid,
+        message: response.body,
+        timestamp: DateTime.now().millisecondsSinceEpoch,
+      );
+
+      // Agregar el mensaje de respuesta de la IA a la lista de mensajes
+      setState(() {
+        messages.add(respuesta.toMap());
+      });
+
+      // Guardar mensajes en Firestore
+      await _fire.guardarMensaje(currentUser!.uid, mensaje);
+      await _fire.guardarMensaje(currentUser!.uid, respuesta);
+      _scrollToBottom();
+    } catch (e) {
+      // Manejo de errores en la solicitud HTTP
+      print("Error al obtener la respuesta de la IA: $e");
+
+      // En caso de error, agrega un mensaje de error
+      setState(() {
+        messages.add({
+          'senderId': 'AI',
+          'receiverId': currentUser!.uid,
+          'message': 'Error: Unable to connect to AI service.',
+          'timestamp': DateTime.now().millisecondsSinceEpoch,
+        });
+      });
+      _scrollToBottom();
     }
   }
+}
+
 
   void _clearMessages() {
     setState(() {
@@ -55,22 +153,23 @@ class _AiHelperViewState extends State<AiHelperView> {
         children: [
           Expanded(
             child: ListView.builder(
+              controller: _scrollController,
               padding: const EdgeInsets.all(10),
               itemCount: messages.length,
               itemBuilder: (context, index) {
                 final message = messages[index];
                 return BubbleSpecialThree(
-                  text: message['text'],
-                  color: message['isUser']
-                      ? const Color(0xFF1B97F3)
-                      : const Color(0xFFE8E8EE),
-                  tail: false,
+                  text: message['message'],
+                  color: message['senderId'] == "AI"
+                      ? const Color(0xFFE8E8EE)
+                      : const Color(0xFF1B97F3),
+                  tail: true,
                   textStyle: const TextStyle(
                     fontFamily: 'WorkSans',
                     fontSize: 14,
                     color: Colors.black,
                   ),
-                  isSender: message['isUser'],
+                  isSender: message['senderId'] != "AI",
                 );
               },
             ),
